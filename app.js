71eb21e2ac343e2b0507d2d389604b83a7146274
pratishelar <pratishshelar961992@gmail.com>
2018-12-18 15:48:11 -0500

    Commit message here

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const  express = require('express');
 const  path = require('path');
 const  cookieParser = require('cookie-parser');
 const  logger = require('morgan');
-require('./app_server/models/db')
+require('./app_api/models/db');
 
 const  indexRouter = require('./app_server/routes/index');
-const  usersRouter = require('./app_server/routes/users');
+//const  usersRouter = require('./app_server/routes/users');
+const  apiRoutes = require('./app_api/routes/index');
 
 const  app = express();
 
@@ -21,7 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
+app.use('/api', apiRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const ctrlLocations = require('../controllers/locations');
 const ctrlOthers = require('../controllers/others');
 
+/* Locations pages */
 router.get('/', ctrlLocations.homelist);
-router.get('/location', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/location/:locationid', ctrlLocations.locationInfo);
+router
+  .route('/location/:locationid/review/new')
+  .get(ctrlLocations.addReview)
+  .post(ctrlLocations.doAddReview);
 
+/* Other pages */
 router.get('/about', ctrlOthers.about);
 
 module.exports = router;
