4a317d3efa6f4363f010b09826ef7933a7050ef5
pratishelar <pratishshelar961992@gmail.com>
2018-12-17 19:33:11 -0500

    Commit message here

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const ctrlLocations = require('../controllers/locations'); 
+const ctrlLocations = require('../controllers/locations');
 const ctrlOthers = require('../controllers/others');
 
-
-/* Locations pages */ 
-router.get('/', ctrlLocations.homelist); 
+router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlLocations.addReview); 
- 
-/* Other pages */ 
-router.get('/about', ctrlOthers.about);   
+router.get('/location/review/new', ctrlLocations.addReview);
 
-
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
