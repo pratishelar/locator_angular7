acb4485e6aeb1db9ba27f0f1b6c88fa01db4b7a8
pratishelar <pratishshelar961992@gmail.com>
2018-12-18 15:50:31 -0500

    Commit message here

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,7 +3,7 @@ const apiOptions = {
   server : 'http://localhost:3000'
 };
 if (process.env.NODE_ENV === 'production') {
-  apiOptions.server = 'https://pure-temple-67771.herokuapp.com';
+  apiOptions.server = 'https://still-woodland-77180.herokuapp.com';
 }
 
 // PUBLIC EXPOSED METHODS
