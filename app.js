90c55a8003cbbf2a2de64a3c7d590056dd5f4019
pratishelar <pratishshelar961992@gmail.com>
2019-01-06 23:37:17 -0500

    make it better

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/api', apiRoutes);
 app.get('*', function(req, res, next) {   
     res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.html'));
 });
-app.use(function(req, res, next) {
+app.use('/api',function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
