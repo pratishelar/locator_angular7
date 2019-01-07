1bb28ddc5718268f38eebdedeffeb3e03b10e114
pratishelar <pratishshelar961992@gmail.com>
2019-01-06 19:56:43 -0500

    make it better

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_public', 'build')));
 
 
-app.use('/api', (req, res, next) => {   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+app.use('/api', (req, res, next) => {   res.header('Access-Control-Allow-Origin', '*');
                                      res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
                                      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');   
                                       res.header('Access-Control-Allow-Credentials', true);
