e5666f13c12e16743eb27bde6227d3d03f49ef1f
pratishelar <pratishshelar961992@gmail.com>
2019-01-06 20:25:56 -0500

    make it better

--- app.js
+++ app.js
@@ -21,6 +21,21 @@ app.all('*', function(req, res, next) {
      next();
 });
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'))
